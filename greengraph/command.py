2d2dabc82f2787b6c0a1bc6a5373daa891db4ce7
third <t.hird.16@ucl.ac.uk>
2017-01-03 12:30:29 +0000

    Create command line file

---
+++ greengraph/command.py
@@ -0,0 +1,23 @@
+__author__ = 'third'
+from argparse import ArgumentParser
+from greengraph.PlotGraph import PlotGraph
+
+
+def process():
+    parser = ArgumentParser(
+        description="Produce a graph of number of green pixels in satellite images between two locations")
+    parser.add_argument("--start", required=True,
+                        help='Starting location')
+    parser.add_argument("--end", required=True,
+                        help='End location')
+    parser.add_argument("--steps", required=True,
+                        help='Number of steps desired between starting and ending locations')
+    parser.add_argument("--out",  required=True,
+                        help="Filename of output")
+
+    arguments = parser.parse_args()
+
+    PlotGraph(arguments.start, arguments.to, arguments.steps, arguments.out)
+
+    if __name__ == "__main__":
+        process()
