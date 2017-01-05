4ec19950753cfa41559ee2d18c420023089c5f27
third <t.hird.16@ucl.ac.uk>
2017-01-05 16:44:44 +0000

    location sequence test complete

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -31,34 +31,49 @@ def test_geolocate():
             for fixture in fixtures:
                 answer = Greengraph(**fixture)
                 answer.geolocate(fixture['start'])
-                mock_geocode.assert_called_with(fixture['start'],exactly_one=False)
+                mock_geocode.assert_called_with(fixture['start'], exactly_one=False)
                 answer.geolocate(fixture['end'])
-                mock_geocode.assert_called_with(fixture['end'],exactly_one=False)
+                mock_geocode.assert_called_with(fixture['end'], exactly_one=False)
+
+
+def test_location_sequence():
+    test_sequence_numpy = np.load(os.path.join(os.path.dirname(__file__), 'ox_london_seq.npy'))
+    with patch.object(geopy.geocoders.GoogleV3, 'geocode') as mock_geocode:
+        answer = Greengraph('Oxford', 'London')
+        mock_seq=answer.location_sequence((51.7520209, -1.2577263),(51.5073509,-0.1277583), 5)
+        npt.assert_array_almost_equal(mock_seq,test_sequence_numpy,decimal=2)
+
+
+def test_green_between():
+    mock_image = open(os.path.join(os.path.dirname(__file__),
+                                   'london_png.png'), 'rb')
 
 
 
 
 
 
-            # class Greengraph(object):
-            # def __init__(self, start, end):
-            # self.start = start
-            # self.end = end
-            # self.geocoder = geopy.geocoders.GoogleV3(
-            # domain="maps.google.co.uk")
-            #
-            # def geolocate(self, place):
-            # return self.geocoder.geocode(place,
-            # exactly_one=False)[0][1]
-            #
-            # def location_sequence(self, start, end, steps):
-            # lats = np.linspace(start[0], end[0], steps)
-            # longs = np.linspace(start[1], end[1], steps)
-            #         return np.vstack([lats, longs]).transpose()
-            #
-            #     def green_between(self, steps):
-            #         return [Map(*location).count_green()
-            #                 for location in self.location_sequence(
-            #                 self.geolocate(self.start),
-            #                 self.geolocate(self.end),
-            #                 steps)]
+
+
+        # class Greengraph(object):
+        # def __init__(self, start, end):
+        # self.start = start
+        # self.end = end
+        # self.geocoder = geopy.geocoders.GoogleV3(
+        # domain="maps.google.co.uk")
+        #
+        # def geolocate(self, place):
+        # return self.geocoder.geocode(place,
+        # exactly_one=False)[0][1]
+        #
+        # def location_sequence(self, start, end, steps):
+        # lats = np.linspace(start[0], end[0], steps)
+        # longs = np.linspace(start[1], end[1], steps)
+        # return np.vstack([lats, longs]).transpose()
+        #
+        # def green_between(self, steps):
+        # return [Map(*location).count_green()
+        # for location in self.location_sequence(
+        # self.geolocate(self.start),
+        # self.geolocate(self.end),
+        # steps)]
