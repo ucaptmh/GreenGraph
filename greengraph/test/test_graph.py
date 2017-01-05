41f2af1ec59e7fd8d60f564c6d98e0e315d22fd2
third <t.hird.16@ucl.ac.uk>
2017-01-05 13:58:47 +0000

    geolocate test added to graph tests

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import numpy.testing as npt
 import yaml
+from mock import Mock, patch
 from nose.tools import assert_equal
 
 
@@ -19,27 +20,45 @@ def test_build_graph():
             answer = Greengraph(**fixture)
             assert_equal(answer.start, fixture['start'])
             assert_equal(answer.end, fixture['end'])
+            assert_equal(answer.geocoder.domain, "maps.google.co.uk")
 
 
-# class Greengraph(object):
-# def __init__(self, start, end):
-# self.start = start
-#         self.end = end
-#         self.geocoder = geopy.geocoders.GoogleV3(
-#             domain="maps.google.co.uk")
-#
-#     def geolocate(self, place):
-#         return self.geocoder.geocode(place,
-#                                      exactly_one=False)[0][1]
-#
-#     def location_sequence(self, start, end, steps):
-#         lats = np.linspace(start[0], end[0], steps)
-#         longs = np.linspace(start[1], end[1], steps)
-#         return np.vstack([lats, longs]).transpose()
-#
-#     def green_between(self, steps):
-#         return [Map(*location).count_green()
-#                 for location in self.location_sequence(
-#                 self.geolocate(self.start),
-#                 self.geolocate(self.end),
-#                 steps)]
+def test_geolocate():
+    with patch.object(geopy.geocoders.GoogleV3, 'geocode') as mock_geocode:
+        with open(os.path.join(os.path.dirname(__file__),
+                               'fixtures', 'build.yaml')) as fixtures_file:
+            fixtures = yaml.load(fixtures_file)
+            for fixture in fixtures:
+                answer = Greengraph(**fixture)
+                answer.geolocate(fixture['start'])
+                mock_geocode.assert_called_with(fixture['start'],exactly_one=False)
+                answer.geolocate(fixture['end'])
+                mock_geocode.assert_called_with(fixture['end'],exactly_one=False)
+
+
+
+
+
+
+            # class Greengraph(object):
+            # def __init__(self, start, end):
+            # self.start = start
+            # self.end = end
+            # self.geocoder = geopy.geocoders.GoogleV3(
+            # domain="maps.google.co.uk")
+            #
+            # def geolocate(self, place):
+            # return self.geocoder.geocode(place,
+            # exactly_one=False)[0][1]
+            #
+            # def location_sequence(self, start, end, steps):
+            # lats = np.linspace(start[0], end[0], steps)
+            # longs = np.linspace(start[1], end[1], steps)
+            #         return np.vstack([lats, longs]).transpose()
+            #
+            #     def green_between(self, steps):
+            #         return [Map(*location).count_green()
+            #                 for location in self.location_sequence(
+            #                 self.geolocate(self.start),
+            #                 self.geolocate(self.end),
+            #                 steps)]
