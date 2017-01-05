25fbe406a0f69f420a06a3b5393883ab2dc44f84
third <t.hird.16@ucl.ac.uk>
2017-01-05 12:17:39 +0000

    Test for graph started

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -3,11 +3,27 @@ __author__ = 'third'
 import numpy as np
 import geopy
 from greengraph.map import Map
+from greengraph.graph import Greengraph
+import os
+import numpy as np
+import numpy.testing as npt
+import yaml
+from nose.tools import assert_equal
+
+
+def test_build_graph():
+    with open(os.path.join(os.path.dirname(__file__),
+                           'fixtures', 'build.yaml')) as fixtures_file:
+        fixtures = yaml.load(fixtures_file)
+        for fixture in fixtures:
+            answer = Greengraph(**fixture)
+            assert_equal(answer.start, fixture['start'])
+            assert_equal(answer.end, fixture['end'])
 
 
 # class Greengraph(object):
-#     def __init__(self, start, end):
-#         self.start = start
+# def __init__(self, start, end):
+# self.start = start
 #         self.end = end
 #         self.geocoder = geopy.geocoders.GoogleV3(
 #             domain="maps.google.co.uk")
