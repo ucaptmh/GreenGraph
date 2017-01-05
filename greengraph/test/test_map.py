63c0f066c6fcff42e58d48b27e0eb0a6fb7da942
third <t.hird.16@ucl.ac.uk>
2017-01-05 11:09:17 +0000

    All maps test added and  working

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -17,14 +17,25 @@ lat = 51.5073509
 long = -0.1277583
 
 
-def test_Map():
+def test_build_map():
+    mock_image = open(os.path.join(os.path.dirname(__file__),
+                                   'london_png.png'), 'rb')
+    with patch('requests.get', return_value=Mock(content=mock_image.read())) as mock_get:
+        test_map = Map(lat, long)
+        mock_get.assert_called_with(
+            'http://maps.googleapis.com/maps/api/staticmap?',
+            params=dict(size='400x400', zoom=10, center='51.5073509,-0.1277583',
+                        style='feature:all|element:labels|visibility:off', sensor='false', maptype='satellite')
+        )
+
+
+def test_read_map():
     mock_image = open(os.path.join(os.path.dirname(__file__),
                                    'london_png.png'), 'rb')
     with patch('requests.get', return_value=Mock(content=mock_image.read())) as mock_get:
         test_map = Map(lat, long)
         npt.assert_allclose(test_map.pixels, np.load(os.path.join(os.path.dirname(__file__),
                                                                   'london_numpy.npy')))
-        # print("map_test")
 
 
 def test_green():
@@ -44,41 +55,14 @@ def test_count_green():
         npt.assert_equal(Map.count_green(test_map, threshold=1.1), 108032, "Error in Map.count_green")
 
 
-        # class Map(object):
-        #
-        # def __init__(self, lat, long, satellite=True,
-        # zoom=10, size=(400, 400), sensor=False):
-        # base = "http://maps.googleapis.com/maps/api/staticmap?"
-        #
-        # params = dict(
-        #     sensor=str(sensor).lower(),
-        #     zoom=zoom,
-        #                 size="x".join(map(str, size)),
-        #                 center=",".join(map(str, (lat, long))),
-        #                 style="feature:all|element:labels|visibility:off"
-        #             )
-        #
-        #             if satellite:
-        #                 params["maptype"] = "satellite"
-        #
-        #             self.image = requests.get(base, params=params).content
-        #             # Fetch our PNG image data
-        #             self.pixels = img.imread(BytesIO(self.image))
-        #             # Parse our PNG image as a numpy array
-        #
-        #     def green(self, threshold):
-        #         # Use NumPy to build an element-by-element logical array
-        #         greener_than_red = self.pixels[:, :, 1] > threshold * self.pixels[:, :, 0]
-        #         greener_than_blue = self.pixels[:, :, 1] > threshold * self.pixels[:, :, 2]
-        #         green = np.logical_and(greener_than_red, greener_than_blue)
-        #         return green
-        #
-        #     def count_green(self, threshold=1.1):
-        #         return np.sum(self.green(threshold))
-        #
-        #     def show_green(data, threshold=1.1):
-        #         green = self.green(threshold)
-        #         out = green[:, :, np.newaxis] * array([0, 1, 0])[np.newaxis, np.newaxis, :]
-        #         buffer = BytesIO()
-        #         result = img.imsave(buffer, out, format='png')
-        #         return buffer.getvalue()
+# show_green isn't actually used at present but is an additional feature.
+# Needed some updating. Tested as follows:
+
+def test_show_green():
+    test_show_green_numpy = np.load(os.path.join(os.path.dirname(__file__), 'london_show_green.npy'))
+    mock_image = open(os.path.join(os.path.dirname(__file__),
+                                   'london_png.png'), 'rb')
+    with patch('requests.get', return_value=Mock(content=mock_image.read())) as mock_get:
+        test_map = Map(lat, long)
+        npt.assert_array_equal(img.imread(BytesIO(Map.show_green(test_map, threshold=1.1))),test_show_green_numpy ,
+                               "Error in Maps.show_green")
